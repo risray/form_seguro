fd69572ca7c9d5ef97892061f41300e5cc6384a9
risray <53979808+risray@users.noreply.github.com>
2019-08-16 20:34:21 -0400

    Add files via upload

---
+++ Areas/Prueba/PruebaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1.Areas.Prueba
+{
+    public class PruebaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Prueba";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Prueba_default",
+                "Prueba/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
